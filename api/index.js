95c5cfbaf1799691ee8768b270a425ff970b56ed
andarpratama <andar.webdev@gmail.com>
2023-02-27 01:24:46 +0700

    api: create auth feature

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
+const authRoute = require('./routes/auth')
 dotenv.config()
 
 const mongoURL = process.env.MONGO_URL_LOCAL
@@ -10,6 +11,8 @@ mongoose.connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(()=> console.log("DB Connection Successfull"))
     .catch(err => console.log(err))
 
-app.listen(8080, ()=> {
-    console.log('Backend server is running!')
+app.use(express.json())
+app.use('/api/auth/', authRoute)
+app.listen(process.env.PORT, ()=> {
+    console.log(`Running on port http://localhost:${process.env.PORT}`)
 })
